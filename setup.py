2a449fb3571c3dcc65fd442fa7f0b383ab5cb91c
Andrew Berger <andbberger@gmail.com>
2018-09-04 23:21:48 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='defez',
+      version='0.1',
+      author='Andrew Berger',
+      author_email='andbberger@gmail.com',
+      url='https://github.com/rueberger/defez',
+      packages=['defez'],
+      install_requires=[])
